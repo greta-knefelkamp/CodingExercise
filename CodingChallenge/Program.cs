140d11897205ac5c673a7956c8dd3b70eaccad55
Greta Knefelkamp <greta.knefelkamp@cdrg.org>
2022-08-05 11:39:21 -0500

    Updates examples to match the ones given

--- CodingChallenge/Program.cs
+++ CodingChallenge/Program.cs
@@ -7,8 +7,8 @@ namespace codingChallenge
         static void Main()
         {
             // creating examples and printing the results
-            List<int> intsToCompare = new() { 1, 300, 2, 6, 12, 9, 231, 500, 3 };
-            int comparisonValue = 5;
+            List<int> intsToCompare = new() { 1, 5, 2, 1, 10 };
+            int comparisonValue = 6;
             Dictionary<string, int> aboveBelowCounts = aboveBelow(intsToCompare, comparisonValue);
             Console.WriteLine("#################################");
             Console.WriteLine($"Number of integers in list above {comparisonValue}: {aboveBelowCounts["above"]}");
@@ -16,10 +16,10 @@ namespace codingChallenge
             Console.WriteLine("#################################");
             Console.WriteLine();
 
-            string originalString = "Hello, World!";
+            string originalString = "MyString";
             Console.WriteLine("#################################");
             Console.WriteLine($"Original string: {originalString}");
-            uint rotationAmount = 6;
+            uint rotationAmount = 2;
             string rotatedString = stringRotation(originalString, rotationAmount);
             Console.WriteLine($"String after rotating to the right by {rotationAmount}: {rotatedString}");
             Console.WriteLine("#################################");
