1faafe2645741f269123edc88f8ae358ee22d80f
Greta Knefelkamp <greta.knefelkamp@cdrg.org>
2022-08-05 10:43:03 -0500

    First commit

---
+++ CodingChallenge/ExampleClass.cs
@@ -0,0 +1,42 @@
+﻿namespace codingChallenge
+{
+    public class exampleClass
+    {
+        /// <summary>
+        /// Compares the numbers in an unsorted list of integers to a given comparison value
+        /// </summary>
+        /// <param name="unsortedIntegers"></param>
+        /// <param name="comparisonInteger"></param>
+        /// <returns>Returns a dictionary with "above"/"below" keys storing the counts
+        /// of integers from the list that were above/below the comparison value</returns>
+        public static Dictionary<string, int> aboveBelow(List<int> unsortedIntegers, int comparisonInteger)
+        {
+            return new Dictionary<string, int>
+            {
+                { "above", unsortedIntegers.Where(x => x > comparisonInteger).Count() },
+                { "below", unsortedIntegers.Where(x => x < comparisonInteger).Count() }
+            };
+        }
+
+        /// <summary>
+        /// Takes a string and rotates it by a given amount of characters
+        /// </summary>
+        /// <param name="stringToRotate"></param>
+        /// <param name="positiveInteger"></param>
+        /// <returns>A new rotated string with the overflow from the original string at the beginning</returns>
+        public static string stringRotation(string stringToRotate, uint positiveInteger)
+        {
+            string rotatedString = stringToRotate;
+            int stringLength = stringToRotate.Length;
+
+            if (stringLength > 0 && positiveInteger > 0)
+            {
+                string originalFirstHalf = stringToRotate[..(int)(stringLength - positiveInteger % stringLength)];
+                string originalSecondHalf = stringToRotate[(int)(stringLength - positiveInteger % stringLength)..];
+                rotatedString = originalSecondHalf + originalFirstHalf;
+            }
+
+            return rotatedString;
+        }
+    }
+}
---
+++ CodingChallenge/Program.cs
@@ -0,0 +1,28 @@
+﻿using static codingChallenge.exampleClass;
+
+namespace codingChallenge
+{
+    class Program
+    {
+        static void Main()
+        {
+            // creating examples and printing the results
+            List<int> intsToCompare = new() { 1, 300, 2, 6, 12, 9, 231, 500, 3 };
+            int comparisonValue = 5;
+            Dictionary<string, int> aboveBelowCounts = aboveBelow(intsToCompare, comparisonValue);
+            Console.WriteLine("#################################");
+            Console.WriteLine($"Number of integers in list above {comparisonValue}: {aboveBelowCounts["above"]}");
+            Console.WriteLine($"Number of integers in list below {comparisonValue}: {aboveBelowCounts["below"]}");
+            Console.WriteLine("#################################");
+            Console.WriteLine();
+
+            string originalString = "Hello, World!";
+            Console.WriteLine("#################################");
+            Console.WriteLine($"Original string: {originalString}");
+            uint rotationAmount = 6;
+            string rotatedString = stringRotation(originalString, rotationAmount);
+            Console.WriteLine($"String after rotating to the right by {rotationAmount}: {rotatedString}");
+            Console.WriteLine("#################################");
+        }
+    }
+}
